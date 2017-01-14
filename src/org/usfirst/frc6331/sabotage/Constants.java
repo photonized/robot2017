fa437c6aff161b2c547e1ba863882ae4faebaf3e
Vlad <vladorigito@gmail.com>
2017-01-14 11:21:41 -0500

    Better commit with leftJoystick called. Also some constants etc. Robot interface could be viewed with SmartDashboard. //TODO: Bind axis or temporary keys to make the robot drive. Basic framework finished.

---
+++ src/org/usfirst/frc6331/sabotage/Constants.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc6331.sabotage;
+
+/**
+ * 
+ * 
+ * @author Vlad
+ * The robot's constants.
+ *
+ */
+public class Constants {
+	public static double SPEED = 0.0;
+	public static double EXPIRATION = 0.1;
+	public static double SENSITIVITY = 0.5;
+	public static double MAX_OUTPUT = 1.0;
+	public static boolean SAFETY = true;
+}
